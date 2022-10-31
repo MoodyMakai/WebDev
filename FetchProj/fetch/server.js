96fbc8dece1593c37a319feaa092c43df6654306
Makai Moody-Broen <moodybroenm@mssm.org>
2022-10-31 08:35:12 -0400

    fetch episode

---
+++ FetchProj/fetch/server.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const app = express()
+const port = 3000
+
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: false}))
+
+function htmlStart(res){
+    res.write(`<!DOCTYPE html>
+    <head>
+        <title>Document</title>
+    </head>
+    <body>
+`);
+}
+
+function htmlEnd(res){(`
+    </body>
+    </html>
+`)}
+
+
+
+/*
+app.get('/', (req,res)=>{
+htmlStart(res);
+res.write(`<h1> cityville </h1>`)
+fetch('https://rickandmortyapi.com/api/character').then(fres => {
+    return fres.json();
+}).then(data =>{
+    for(let person of data.results){
+        res.write(`<h1>${person.name}</h1>`);
+    }
+    htmlEnd(res);
+})
+.catch(err =>{
+res.write(`<h1> uh oh ${err.message}</h1>`)})
+})*/
+
+app.get('/', async (req,res)=>{
+    htmlStart(res);
+    res.write(`<h1> cityville </h1>`)
+    let fRes = await fetch('https://rickandmortyapi.com/api/character')
+    console.log(fRes)
+    let data = await fRes.json(); 
+    for (let person of data.results){
+        res.write(`<h1>${person.name}</h1>`)
+        let eRes = await fetch('https://rickandmortyapi.com/api/episode')
+        let data2 = await eRes.json()
+            for(episode of data2.results){
+                res.write(`<h1>${episode.name}`)}
+    }})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`server on ${port}`)
+});

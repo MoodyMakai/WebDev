b0242aa70b6fe6ff36faf95c7fa008e94908e2f9
Makai Moody-Broen <moodybroenm@mssm.org>
2022-11-14 08:32:53 -0500

    BlackJack

--- BlackJack/API/BJAPI.js
+++ BlackJack/API/BJAPI.js
@@ -5,14 +5,19 @@ app.use(express.static('public'))
 app.use(express.urlencoded({extended: false}))
 
 
-let deck = [1,2,3,4,5,,6,7,8,9,10,11,12,13];
-
+let deck = [{ Hearts: [1,2,3,4,5,6,7,8,9,10,11,12,13],
+              Clubs: [1,2,3,4,5,6,7,8,9,10,11,12,13],
+              Spades: [1,2,3,4,5,6,7,8,9,10,11,12,13],
+              Diamonds: [1,2,3,4,5,6,7,8,9,10,11,12,13]
+            }]
 let game = {
     plyerHand: [],
     dealerHand: []
 }
 
 function restartGame(){
+    //set pDeck to 0
+    //set gDeck to default
     
 }
 

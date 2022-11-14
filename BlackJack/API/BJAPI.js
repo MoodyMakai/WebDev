42eb9ec4c78baff851bc4fe84a129351b078b28d
Makai Moody-Broen <moodybroenm@mssm.org>
2022-11-14 16:59:30 -0500

    api half done

--- BlackJack/API/BJAPI.js
+++ BlackJack/API/BJAPI.js
@@ -5,33 +5,71 @@ app.use(express.static('public'))
 app.use(express.urlencoded({extended: false}))
 
 
-let deck = [{ Hearts: [1,2,3,4,5,6,7,8,9,10,11,12,13],
-              Clubs: [1,2,3,4,5,6,7,8,9,10,11,12,13],
-              Spades: [1,2,3,4,5,6,7,8,9,10,11,12,13],
-              Diamonds: [1,2,3,4,5,6,7,8,9,10,11,12,13]
-            }]
-let game = {
-    plyerHand: [],
-    dealerHand: []
+let deck = [
+            {rank: '', suite: ""}
+        ]  
+function deckBuild(){
+    n=0;
+    s = 0;
+    d = 0;
+    c = 0;
+    for(x of deck){
+        n++
+        if(n < 14){
+        deck.push({
+            rank: n,
+            suite: "heart"
+        })
+        } else{
+            s++
+            if(s < 14){
+                deck.push({
+                    rank: s,
+                    suite: "spades"
+                })
+                }else {
+                    d++
+                    if(d < 14){
+                        deck.push({
+                            rank: d,
+                            suite: "diamonds"
+                        })
+                }else{
+                    c++
+                    if(c < 14){
+                        deck.push({
+                            rank: c,
+                            suite: "clubs"
+                        })}}
+                }
+        }
+    }
+        
 }
+deckBuild()
+console.log(deck)
+let game = [{
+    playerHand: '',
+    dealerHand: ''
+}]
 
-function restartGame(){
-    //set pDeck to 0
-    //set gDeck to default
     
-}
 
-function shuffleDeck(){
-    
-}
 
 function dealCard(){
-    
+    z=0
+    for (x of game){
+        z++
+        if (z<3){
+            s = Math.floor(Math.random()*52)
+            game.push({
+            playerHand: deck[s] 
+        }) 
+        console.log(s)  
+    }} 
 }
-
-
-
-
+dealCard()
+console.log(game)
 
 
 
